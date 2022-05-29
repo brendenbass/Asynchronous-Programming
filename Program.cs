9cde9ee4f4dce52606a9ffa9dfbb5e230281f90d
Brenden Bass <80184025+brendenbass@users.noreply.github.com>
2022-05-28 21:05:59 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Asynchronous_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 200000;
+            char selectedChar = 'r';
+
+            Task<string> t = ConcatenateCharsAsync(selectedChar, count);
+
+            Console.WriteLine("In Progress.");
+            Thread.Sleep(333);
+            Console.Clear();
+            Console.WriteLine("In Progress..");
+            Thread.Sleep(333);
+            Console.Clear();
+            Console.WriteLine("In Progress...");
+
+            Console.WriteLine($"The length of the result completed is {t.Result.Length}.");
+        }
+
+        public static string ConcatenateChars(char charToConcatenate, int count)
+        {
+            string concatenatedString = String.Empty;
+
+            for (int i = 0; i < count; i++)
+            {
+                concatenatedString += charToConcatenate;
+            }
+            return concatenatedString;
+        }
+
+        public async static Task<string> ConcatenateCharsAsync(char charToConcatenate, int count)
+        {
+            return await Task.Factory.StartNew(() =>
+            {
+                return ConcatenateChars(charToConcatenate, count);
+            });
+        }
+    }
+}
